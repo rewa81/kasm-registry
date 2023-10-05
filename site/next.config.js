45283b5ec1aaf108b1d33af0f7c0ff4eff7c5eba
René <69624767+rewa81@users.noreply.github.com>
2023-10-05 13:59:59 +0200

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -2,11 +2,11 @@
 
 const nextConfig = {
   env: {
-    name: 'Kasm Technologies',
-    description: 'The official store for Kasm supported workspaces.',
+    name: 'rwcloud.ch',
+    description: 'Eigene KASM Regsitry.',
     icon: '/img/logo.svg',
-    listUrl: 'https://registry.kasmweb.com/',
-    contactUrl: 'https://kasmweb.com/support',
+    listUrl: 'https://rewa81.github.io/kasm-registry/',
+    contactUrl: 'https://rewa81.github.io/kasm-registry/issues',
   },
   reactStrictMode: true,
   swcMinify: true,
